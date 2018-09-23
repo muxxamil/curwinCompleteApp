d24203649babdc0c2dcd6649a638f75db9a100fb
muxamil <muzamilejaz@zameen.com>
2018-09-24 01:30:08 +0500

    api_url is replaced

--- admin/config/defaults.php
+++ admin/config/defaults.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_URL = 'localhost:3002';
-// $API_URL = '162.255.85.99:3001';
+// $API_URL = 'localhost:3002';
+$API_URL = '162.255.85.99:3001';
 $IMAGE_PREFIX = 'http://www.curwinbusinesscentre.com/img/landing/';
 
 $PRIVILEGES = array(
