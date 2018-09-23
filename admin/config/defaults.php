a0bce08ab99ff03dbed5fa83f056169f35265fd3
muxamil <muzamilejaz@zameen.com>
2018-09-24 01:29:11 +0500

    bug fix

--- admin/config/defaults.php
+++ admin/config/defaults.php
@@ -1,8 +1,17 @@
 <?php
 
-// $API_URL = 'localhost:3002';
-$API_URL = '162.255.85.99:3001';
+$API_URL = 'localhost:3002';
+// $API_URL = '162.255.85.99:3001';
 $IMAGE_PREFIX = 'http://www.curwinbusinesscentre.com/img/landing/';
+
+$PRIVILEGES = array(
+	"CAN_SEE_DASHBOARD" 		=> "CSD",
+	"CAN_MANAGE_USER" 			=> "CMU",
+	"CAN_MANAGE_BLOG" 			=> "CMB",
+	"CAN_MANAGE_SCHEDULE" 		=> "CMS",
+	"CAN_MANAGE_ALL_SCHEDULE" 	=> "CMAS",
+	"CAN_MANAGE_MY_SCHEDULE" 	=> "CMMS"
+);
 
 $DEFINITION_PRIVILEGES = array(
 	"Dashboard" => "CSD",
--- admin/controllers/ajax/get_booking_list.php
+++ admin/controllers/ajax/get_booking_list.php
@@ -8,7 +8,7 @@
 	    session_start();
 	}
 
-	if(!empty($_GET['onlyLoggedInUser']) && $_GET['onlyLoggedInUser'] == true) {
+	if(!empty($_GET['onlyLoggedInUser']) && $_GET['onlyLoggedInUser'] == true && !in_array($PRIVILEGES['CAN_MANAGE_ALL_SCHEDULE'], $_SESSION['privileges'])) {
 		$_GET['bookedBy'] = $_SESSION['userInfo']->id;
 	}
 
